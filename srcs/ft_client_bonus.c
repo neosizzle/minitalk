090bbfdfe47119625774caa721e5493e2fb93099
neosizzle <junahnngd1115@gmail.com>
2021-08-14 16:01:14 +0800

    change

--- srcs/ft_client_bonus.c
+++ srcs/ft_client_bonus.c
@@ -1,6 +1,6 @@
 #include "minitalk_bonus.h"
 
-static int	g_total_bytes;
+static char	*g_message;
 
 static void send_bits(int pid, char c)
 {
@@ -26,7 +26,6 @@ static void	send_message(int pid, char *message)
 	while (message[++i])
 		send_bits(pid, message[i]);
 	send_bits(pid, message[i]);
-	g_total_bytes += i;
 }
 
 static void	handle_sig(int sig)
@@ -34,7 +33,7 @@ static void	handle_sig(int sig)
 	if (sig == SIGUSR1)
 	{
 		ft_putstr_fd("\nTotal bytes acknowledged : ", 1);
-		ft_putnbr_fd(g_total_bytes, 1);
+		ft_putnbr_fd(ft_strlen(g_message), 1);
 	}
 }
 
@@ -42,14 +41,12 @@ int main(int argc, char *argv[])
 {
 	(void) argv;
 	int		pid;
-	char	*message;
 	struct	 sigaction sa;
 
 	if (argc == 3)
 	{
-		g_total_bytes = 0;
 		pid = ft_atoi(argv[1]);
-		message = argv[2];
+		g_message = argv[2];
 		sa.sa_handler = handle_sig;
 		sigaction(SIGUSR1, &sa, NULL);
 		sigaction(SIGUSR2, &sa, NULL);
